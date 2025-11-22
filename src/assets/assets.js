151270c39a0b1a005b436e35d3938d65a5e937a3
Awais Ali <awaisali.bscsf22@iba-suk.edu.pk>
2025-11-22 14:22:47 +0500

    Initial commit - Spotify Clone React project

---
+++ src/assets/assets.js
@@ -0,0 +1,536 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpeg'
+import img2 from './img2.jpeg'
+import img3 from './img3.jpeg'
+import img4 from './img4.jpeg'
+import img5 from './img5.jpeg'
+import img6 from './img6.jpeg'
+import img7 from './img7.jpeg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpeg'
+import img10 from './img10.jpeg'
+import img11 from './img11.jpeg'
+import img12 from './img12.jpeg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import img17 from './img17.jpeg'
+import img18 from './img18.jpeg'
+import img19 from './img19.jpeg'
+import img20 from './img20.jpeg'
+import img21 from './img21.jpeg'
+import img22 from './img22.jpeg'
+import song1 from  './JO_TUM_MERE_HO.mp3'
+import song2 from  './JHOL.mp3'
+import song3 from  './SAMJHO_NA.mp3'
+import song4 from  './SOFTLY.mp3'
+import song5 from  './TU_HAI_KAHAN.mp3'
+import song6 from  './WISHES.mp3'
+import song7 from  './BORN_TO_SHINE.mp3'
+import song8 from  './CHAL_DEYE_TUM_KAHAN.mp3'
+import song9 from  './DIL_DIYAN_GALLAN.mp3'
+import song10 from  './SATRANGA.mp3'
+import song11 from  './KAHANI_SUNO_2.0.mp3'
+import song12 from  './BEMATLAB.mp3'
+import song13 from  './DIE_WITH_A_SMILE.mp3'
+import song14 from  './BIRDS_OF_A_FEATHER.mp3'
+
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {
+        id:1,
+        name: "Top 50 Pakistan",
+        image: img9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#FFFF99"
+    },
+    {
+        id:2,
+        name: "Trending Pakistan",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#39319e99"
+    },
+    {
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {
+        id:4,
+        name: "Mega Hits,",
+        image: img11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {
+        id:5,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Jo Tum Mere HO",
+        image: img1,
+        file:song1,
+        desc:"Anuv Jain",
+        duration:"4:17"
+    },
+    {
+        id:1,
+        name: "Jhol",
+        image: img2,
+        file:song2,
+        desc:"Maanu, Annural Khalid",
+        duration:"4:38"
+    },
+    {
+        id:2,
+        name: "Samjho Na",
+        image: img3,
+        file:song3,
+        desc:"Aditya Rikhari ",
+        duration:"2:49"
+    },
+    {
+        id:3,
+        name: "Softly",
+        image: img4,
+        file:song4,
+        desc:"Karan Aujla,Ikky",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Tu Hai Kahan",
+        image: img5,
+        file:song5,
+        desc:"AUR",
+        duration:"4:23"
+    },
+    {
+        id:5,
+        name: "Wishes",
+        image: img6    ,
+        file:song6,
+        desc:"Hasan Raheem, Umair, Talwiinder",
+        duration:"3:38"
+    },
+    {
+        id:6,
+        name: "Born To Shine",
+        image: img7,
+        file:song7,
+        desc:"Diljit Dosanjh",
+        duration:"3:37"
+    },
+    {
+        id:7,
+        name: "Chal Deye Tum",
+        image: img12,
+        file:song8,
+        desc:"AUR",
+        duration:"4:35"
+    },
+    {
+        id:8,
+        name: "Dil Diyan Gallan",
+        image: img17,
+        file:song9,
+        desc:"Atif Aslam",
+        duration:"3:04"
+    },
+    {
+        id:9,
+        name: "Satranga",
+        image: img18,
+        file:song10,
+        desc:"Arjit Singh",
+        duration:"4:32"
+    },
+    {
+        id:10,
+        name: "Kahani Suno 2.0",
+        image: img19,
+        file:song11,
+        desc:"Kaifi Khalil",
+        duration:"2:53"
+    },
+    {
+        id:11,
+        name: "Bematlab",
+        image: img20,
+        file:song12,
+        desc:"Asim Azhar, Talha Anjum",
+        duration:"4:11"
+    },
+    {
+        id:12,
+        name: "Die With A Smile",
+        image: img21,
+        file:song13,
+        desc:"Lady Gaga, Bruno Mars",
+        duration:"4:12"
+    },
+    {
+        id:13,
+        name: "Birds Of A Feather",
+        image: img22,
+        file:song14,
+        desc:"Billie Eilish",
+        duration:"3:50"
+    },
+    {
+        id:0,
+        name: "Jo Tum Mere HO",
+        image: img1,
+        file:song1,
+        desc:"Anuv Jain",
+        duration:"4:17"
+    },
+    {
+        id:1,
+        name: "Jhol",
+        image: img2,
+        file:song2,
+        desc:"Maanu, Annural Khalid",
+        duration:"4:38"
+    },
+    {
+        id:2,
+        name: "Samjho Na",
+        image: img3,
+        file:song3,
+        desc:"Aditya Rikhari ",
+        duration:"2:49"
+    },
+    {
+        id:3,
+        name: "Softly",
+        image: img4,
+        file:song4,
+        desc:"Karan Aujla,Ikky",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Tu Hai Kahan",
+        image: img5,
+        file:song5,
+        desc:"AUR",
+        duration:"4:23"
+    },
+    {
+        id:5,
+        name: "Wishes",
+        image: img6    ,
+        file:song6,
+        desc:"Hasan Raheem, Umair, Talwiinder",
+        duration:"3:38"
+    },
+    {
+        id:6,
+        name: "Born To Shine",
+        image: img7,
+        file:song7,
+        desc:"Diljit Dosanjh",
+        duration:"3:37"
+    },
+    {
+        id:7,
+        name: "Chal Deye Tum",
+        image: img12,
+        file:song8,
+        desc:"AUR",
+        duration:"4:35"
+    },
+    {
+        id:8,
+        name: "Dil Diyan Gallan",
+        image: img17,
+        file:song9,
+        desc:"Atif Aslam",
+        duration:"3:04"
+    },
+    {
+        id:9,
+        name: "Satranga",
+        image: img18,
+        file:song10,
+        desc:"Arjit Singh",
+        duration:"4:32"
+    },
+    {
+        id:10,
+        name: "Kahani Suno 2.0",
+        image: img19,
+        file:song11,
+        desc:"Kaifi Khalil",
+        duration:"2:53"
+    },
+    {
+        id:11,
+        name: "Bematlab",
+        image: img20,
+        file:song12,
+        desc:"Asim Azhar, Talha Anjum",
+        duration:"4:11"
+    },
+    {
+        id:12,
+        name: "Die With A Smile",
+        image: img21,
+        file:song13,
+        desc:"Lady Gaga, Bruno Mars",
+        duration:"4:12"
+    },
+    {
+        id:13,
+        name: "Birds Of A Feather",
+        image: img22,
+        file:song14,
+        desc:"Billie Eilish",
+        duration:"3:50"
+    },
+    {
+        id:0,
+        name: "Jo Tum Mere HO",
+        image: img1,
+        file:song1,
+        desc:"Anuv Jain",
+        duration:"4:17"
+    },
+    {
+        id:1,
+        name: "Jhol",
+        image: img2,
+        file:song2,
+        desc:"Maanu, Annural Khalid",
+        duration:"4:38"
+    },
+    {
+        id:2,
+        name: "Samjho Na",
+        image: img3,
+        file:song3,
+        desc:"Aditya Rikhari ",
+        duration:"2:49"
+    },
+    {
+        id:3,
+        name: "Softly",
+        image: img4,
+        file:song4,
+        desc:"Karan Aujla,Ikky",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Tu Hai Kahan",
+        image: img5,
+        file:song5,
+        desc:"AUR",
+        duration:"4:23"
+    },
+    {
+        id:5,
+        name: "Wishes",
+        image: img6    ,
+        file:song6,
+        desc:"Hasan Raheem, Umair, Talwiinder",
+        duration:"3:38"
+    },
+    {
+        id:6,
+        name: "Born To Shine",
+        image: img7,
+        file:song7,
+        desc:"Diljit Dosanjh",
+        duration:"3:37"
+    },
+    {
+        id:7,
+        name: "Chal Deye Tum",
+        image: img12,
+        file:song8,
+        desc:"AUR",
+        duration:"4:35"
+    },
+    {
+        id:8,
+        name: "Dil Diyan Gallan",
+        image: img17,
+        file:song9,
+        desc:"Atif Aslam",
+        duration:"3:04"
+    },
+    {
+        id:9,
+        name: "Satranga",
+        image: img18,
+        file:song10,
+        desc:"Arjit Singh",
+        duration:"4:32"
+    },
+    {
+        id:10,
+        name: "Kahani Suno 2.0",
+        image: img19,
+        file:song11,
+        desc:"Kaifi Khalil",
+        duration:"2:53"
+    },
+    {
+        id:11,
+        name: "Bematlab",
+        image: img20,
+        file:song12,
+        desc:"Asim Azhar, Talha Anjum",
+        duration:"4:11"
+    },
+    {
+        id:12,
+        name: "Die With A Smile",
+        image: img21,
+        file:song13,
+        desc:"Lady Gaga, Bruno Mars",
+        duration:"4:12"
+    },
+    {
+        id:13,
+        name: "Birds Of A Feather",
+        image: img22,
+        file:song14,
+        desc:"Billie Eilish",
+        duration:"3:50"
+    },
+    {
+        id:0,
+        name: "Jo Tum Mere HO",
+        image: img1,
+        file:song1,
+        desc:"Anuv Jain",
+        duration:"4:17"
+    },
+    {
+        id:1,
+        name: "Jhol",
+        image: img2,
+        file:song2,
+        desc:"Maanu, Annural Khalid",
+        duration:"4:38"
+    },
+    {
+        id:2,
+        name: "Samjho Na",
+        image: img3,
+        file:song3,
+        desc:"Aditya Rikhari ",
+        duration:"2:49"
+    },
+    {
+        id:3,
+        name: "Softly",
+        image: img4,
+        file:song4,
+        desc:"Karan Aujla,Ikky",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Tu Hai Kahan",
+        image: img5,
+        file:song5,
+        desc:"AUR",
+        duration:"4:23"
+    },
+    {
+        id:5,
+        name: "Wishes",
+        image: img6    ,
+        file:song6,
+        desc:"Hasan Raheem, Umair, Talwiinder",
+        duration:"3:38"
+    },
+    {
+        id:6,
+        name: "Born To Shine",
+        image: img7,
+        file:song7,
+        desc:"Diljit Dosanjh",
+        duration:"3:37"
+    },
+    {
+        id:7,
+        name: "Chal Deye Tum",
+        image: img12,
+        file:song8,
+        desc:"AUR",
+        duration:"4:35"
+    }
+]
